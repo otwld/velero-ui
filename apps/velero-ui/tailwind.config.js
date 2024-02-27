b9e84b4ffaed69e17b8c1c3899f8631648fb5749
Jean-Baptiste DETROYES <jean-baptiste@detroyes.fr>
2024-02-27 16:40:29 +0100

    feat: update tailwind configuration

--- apps/velero-ui/tailwind.config.js
+++ apps/velero-ui/tailwind.config.js
@@ -4,7 +4,7 @@ const { join } = require('path');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '../../node_modules/flowbite/**/*.js',
+    '../../node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
     join(__dirname, 'index.html'),
     join(__dirname, 'src/**/*!(*.stories|*.spec).{vue,ts,tsx,js,jsx}'),
     ...createGlobPatternsForDependencies(__dirname),
