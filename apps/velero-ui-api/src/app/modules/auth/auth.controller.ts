f330379311e45ea6b4ea358abf7d036afa05519d
Jean-Baptiste DETROYES <jean-baptiste@detroyes.fr>
2024-05-17 17:32:16 +0800

    lint(apps/velero-ui-api): apply lint

--- apps/velero-ui-api/src/app/modules/auth/auth.controller.ts
+++ apps/velero-ui-api/src/app/modules/auth/auth.controller.ts
@@ -1,19 +1,12 @@
-import {
-  Body,
-  Controller,
-  Post,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from '@velero-ui-api/modules/auth/auth.service';
 import { Public } from '@velero-ui-api/shared/decorators/public.decorator';
-import {AuthGuard} from "@nestjs/passport";
+import { AuthGuard } from '@nestjs/passport';
 
 @Public()
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(AuthGuard('local'))
   @Post('/login')
