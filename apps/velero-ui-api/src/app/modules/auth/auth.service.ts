990ced18d6ae6d5cbcf523f76c8e140643d59f29
Jean-Baptiste DETROYES <jean-baptiste@detroyes.fr>
2024-06-05 19:46:33 +0800

    chore(velero-ui-api): remove app logger

--- apps/velero-ui-api/src/app/modules/auth/auth.service.ts
+++ apps/velero-ui-api/src/app/modules/auth/auth.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { JwtService } from '@nestjs/jwt';
+import {Injectable} from '@nestjs/common';
+import {ConfigService} from '@nestjs/config';
+import {JwtService} from '@nestjs/jwt';
+// import {AppLogger} from "@velero-ui-api/shared/modules/logger/logger.service";
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly configService: ConfigService,
     private readonly jwtService: JwtService
-  ) {}
+  ) {
+  }
 
   public validateBasicUser(username: string, password: string): boolean {
     const {
@@ -20,7 +22,9 @@ export class AuthService {
   }
 
   public login(username: string) {
-    const payload = { name: username, sub: 'local' };
+    const payload = {name: username, sub: 'local'};
+
+    // this.appLogger.debug(`Admin login: ${username}`);
     return {
       access_token: this.jwtService.sign(payload),
     };
