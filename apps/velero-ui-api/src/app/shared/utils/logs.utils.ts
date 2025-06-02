eacfe6d14ad027a0de2c83f7a7e2e663fb233790
Jean-Baptiste DETROYES <jean-baptiste@detroyes.fr>
2025-06-02 21:01:47 +0800

    chore: fix parseLog
    Signed-off-by: Jean-Baptiste DETROYES <jean-baptiste@detroyes.fr>

--- apps/velero-ui-api/src/app/shared/utils/logs.utils.ts
+++ apps/velero-ui-api/src/app/shared/utils/logs.utils.ts
@@ -7,16 +7,26 @@ const execRegex = (regex: RegExp, line: string): string | null => {
     return result[1].replace(/velero\//, '').replace(/\/plugins\//, '');
   }
   return null;
-}
+};
 
-const parseValueGetters: Record<string, (line: string) => string | null>  = {
+const parseValueGetters: Record<string, (line: string) => string | null> = {
   plugin: (line) => execRegex(/plugin=([^\s]+)/, line),
-  backupStorageLocation: (line) => execRegex(/backup-storage-location=([^\s]+)/, line) || execRegex(/backupLocation=([^\s]+)/, line) || execRegex(/bsl=([^\s]+)/, line),
-  volumeSnapshotLocation: (line) => execRegex(/volume-snapshot-location=([^\s]+)/, line) || execRegex(/volumeSnapshotLocation=([^\s]+)/, line) || execRegex(/vsl=([^\s]+)/, line),
+  backupStorageLocation: (line) =>
+    execRegex(/backup-storage-location=([^\s]+)/, line) ||
+    execRegex(/backupLocation=([^\s]+)/, line) ||
+    execRegex(/bsl=([^\s]+)/, line),
+  volumeSnapshotLocation: (line) =>
+    execRegex(/volume-snapshot-location=([^\s]+)/, line) ||
+    execRegex(/volumeSnapshotLocation=([^\s]+)/, line) ||
+    execRegex(/vsl=([^\s]+)/, line),
   backup: (line) => execRegex(/backup=([^\s]+)/, line),
   backupRepository: (line) => execRegex(/backupRepo=([^\s]+)/, line),
-  podVolumeBackup: (line) => execRegex(/podvolumebackup=([^\s]+)/, line) || execRegex(/pvb=([^\s]+)/, line),
-  podVolumeRestore: (line) => execRegex(/podvolumerestore=([^\s]+)/, line) || execRegex(/pvr=([^\s]+)/, line),
+  podVolumeBackup: (line) =>
+    execRegex(/podvolumebackup=([^\s]+)/, line) ||
+    execRegex(/pvb=([^\s]+)/, line),
+  podVolumeRestore: (line) =>
+    execRegex(/podvolumerestore=([^\s]+)/, line) ||
+    execRegex(/pvr=([^\s]+)/, line),
   namespace: (line) => execRegex(/namespace=([^\s]+)/, line),
   resource: (line) => execRegex(/resource=([^\s]+)/, line),
   group: (line) => execRegex(/group=([^\s]+)/, line),
@@ -24,56 +34,70 @@ const parseValueGetters: Record<string, (line: string) => string | null>  = {
   logSource: (line) => execRegex(/logSource="([^"]+)"/, line),
   error: (line) => execRegex(/error="([^"]+)"/, line),
   expiration: (line) => execRegex(/error="([^"]+)"/, line),
-  deleteBackupRequest: (line) => execRegex(/deleteBackupRequest=([^\s]+)/, line),
-  serverStatusRequest: (line) => execRegex(/serverStatusRequest=([^\s]+)/, line),
+  deleteBackupRequest: (line) =>
+    execRegex(/deleteBackupRequest=([^\s]+)/, line),
+  serverStatusRequest: (line) =>
+    execRegex(/serverStatusRequest=([^\s]+)/, line),
   downloadRequest: (line) => execRegex(/downloadRequest=([^\s]+)/, line),
-}
+};
 
 export const parseVeleroLog = (line: string): VeleroLog => {
-  const regex = /time="([^"]+)"\s+level=([^\s]+)\s+msg="([^"]+)"/;
+  try {
+    const regex = /time="([^"]+)"\s+level=([^\s]+)\s+msg="([^"]+)"/;
 
-  const match = regex.exec(line);
+    const match = regex.exec(line);
 
-  if (!match) {
-    return null;
-  }
-
-  const [, time, level, msg] = match;
-
-  const object = {
-    date: time,
-    level,
-    message: msg,
-    raw: line,
-  }
-
-  Object.entries(parseValueGetters).forEach(([key, value]) => {
-    const getter = parseValueGetters[key];
-
-    if (!getter) {
+    if (!match) {
       return null;
     }
 
-    const val = getter(line);
-    if (val) {
-      object[key] = val;
-    }
+    const [, time, level, msg] = match;
 
-  });
-  return object;
+    const object = {
+      date: time,
+      level,
+      message: msg,
+      raw: line,
+    };
+
+    Object.entries(parseValueGetters).forEach(([key, value]) => {
+      const getter = parseValueGetters[key];
+
+      if (!getter) {
+        return null;
+      }
+
+      const val = getter(line);
+      if (val) {
+        object[key] = val;
+      }
+    });
+
+    return object;
+  } catch (error) {
+    return null;
+  }
 };
 
 export const parseVeleroUILog = (line: string): Log => {
-  const object = JSON.parse(line);
-
-  if (!object) {
+  if (!line) {
     return null;
   }
 
-  return {
-    date: object.timestamp,
-    level: object.level,
-    message: object.message,
-    raw: line,
+  try {
+    const object = JSON.parse(line);
+
+    if (!object) {
+      return null;
+    }
+
+    return {
+      date: object.timestamp,
+      level: object.level,
+      message: object.message,
+      raw: line,
+    };
+  } catch (error) {
+    return null;
   }
-}
+};
