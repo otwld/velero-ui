e572ac0d5ab8df185a2b7be25469f371e09de15d
Jean-Baptiste DETROYES <jean-baptiste@detroyes.fr>
2025-06-27 11:38:47 +0800

    fix: add rejectUnauthorized in https agent
    Signed-off-by: Jean-Baptiste DETROYES <jean-baptiste@detroyes.fr>

--- apps/velero-ui-api/src/app/modules/backup/backup.module.ts
+++ apps/velero-ui-api/src/app/modules/backup/backup.module.ts
@@ -4,9 +4,18 @@ import { BackupController } from './backup.controller';
 import { HttpModule } from '@nestjs/axios';
 import { DeleteBackupRequestModule } from '@velero-ui-api/modules/delete-backup-request/delete-backup-request.module';
 import { DownloadRequestModule } from '@velero-ui-api/modules/download-request/download-request.module';
+import https from 'https';
 
 @Module({
-  imports: [HttpModule, DeleteBackupRequestModule, DownloadRequestModule],
+  imports: [
+    HttpModule.register({
+      httpsAgent: new https.Agent({
+        rejectUnauthorized: false,
+      }),
+    }),
+    DeleteBackupRequestModule,
+    DownloadRequestModule,
+  ],
   providers: [BackupService],
   controllers: [BackupController],
   exports: [BackupService],
